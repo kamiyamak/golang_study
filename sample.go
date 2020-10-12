9cc17a73af6f743895d4cd6ac39c6ea32a85b229
kamiyamak <kamiyamak@istyle.co.jp>
2020-10-12 18:30:46 +0900

    kinocode-06

---
+++ sample.go
@@ -0,0 +1,16 @@
+package main // mainパッケージであることを宣言
+
+import (
+	"fmt"
+) // fmtモジュールをインポート
+
+func main() { // 最初に実行されるmain()関数を定義 エントリーポイント
+	num2 := 2
+	NUM2 := 2
+	fmt.Println(num2)
+	fmt.Println(NUM2)
+
+	fmt.Println("Greetings")
+	fmt.Println("hello, world")
+	fmt.Println("Good morning")
+}
