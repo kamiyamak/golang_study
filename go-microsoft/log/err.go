1426e2666deb3a4b0585b79663d4d4791943be11
kamiyamak <kamiyamak@istyle.co.jp>
2021-09-27 00:54:15 +0900

    2021-09-27

---
+++ go-microsoft/log/err.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Employee struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Address   string
+}
+
+var ErrNotFound = errors.New("Employee not found!")
+
+func main() {
+	employee, err := getInformation(1000)
+	if errors.Is(err, ErrNotFound) {
+		fmt.Printf("NOT FOUND: %v\n", err)
+	} else {
+		fmt.Print(employee)
+	}
+}
+
+func getInformation(id int) (*Employee, error) {
+	if id != 1001 {
+		return nil, ErrNotFound
+	}
+
+	employee := Employee{LastName: "Doe", FirstName: "John"}
+	return &employee, nil
+}
+
+func apiCallEmployee(id int) (*Employee, error) {
+	employee := Employee{LastName: "Doe", FirstName: "John"}
+	return &employee, nil
+}
